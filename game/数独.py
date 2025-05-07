89e001bc908b6d332949d7d73e57c4daa6e88ea6
mhstudy <153063151+mhstudy@users.noreply.github.com>
2025-05-07 15:35:14 +0800

    Create 数独.py

---
+++ game/数独.py
@@ -0,0 +1,86 @@
+def is_valid(board, row, col, num):
+    # 检查行是否有重复
+    for x in range(9):
+        if board[row][x] == num:
+            return False
+
+    # 检查列是否有重复
+    for x in range(9):
+        if board[x][col] == num:
+            return False
+
+    # 检查小宫格是否有重复
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+    return True
+
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return (None, None)
+
+
+def solve_sudoku(board):
+    row, col = find_empty(board)
+    if row is None:
+        return True  # 数独已解
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0  # 回溯
+    return False
+
+
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            print(board[i][j], end=" ")
+        print()
+
+
+# 测试数独题目
+sudoku_board = [
+
+    [0, 0, 0, 0, 5, 0, 0, 0, 2],
+    [2, 4, 0, 9, 0, 0, 0, 8, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 3, 0, 2, 0, 4, 0, 9, 6],
+    [0, 1, 0, 0, 0, 0, 4, 0, 0],
+    [0, 0, 0, 0, 0, 9, 0, 2, 0],
+    [0, 0, 0, 1, 0, 0, 6, 0, 0],
+    [0, 0, 0, 4, 9, 0, 0, 0, 5],
+    [8, 0, 3, 0, 0, 6, 0, 0, 0]
+
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+]
+
+if solve_sudoku(sudoku_board):
+    print("数独已解，结果如下:")
+    print_board(sudoku_board)
+else:
+    print("无解")
